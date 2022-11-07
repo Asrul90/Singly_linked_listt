df0161915399ec1e8d64caf8115514e3e9a8e9a0
M S I <M S I@MSI>
2022-11-07 17:04:01 +0700

    add note list

---
+++ Singly_linked_list/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_linked_list
+{
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+    class list
+    {
+        Node START;
+        
+        public list()
+        {
+            START = null;
+        }
+        
+        public void addNote()// add note list
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of thr student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
